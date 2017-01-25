de040500de6f364c06e1a4d433c8f33fa2a5734c
Pelle Braendgaard <pelle@stakeventures.com>
2017-01-24 20:49:05 -0600

    Signing of JWT
    Still untested, thus WIP. Need testing setup.

--- src/JWT.js
+++ src/JWT.js
@@ -1,13 +1,22 @@
+import { TokenSigner, createUnsignedToken, TokenVerifier, decodeToken } from 'json-tokens' 
 
-export function createJwT ({address, signer}, payload, callback) {
+const JOSE_HEADER = {typ: 'JWT', alg: 'ES256K'}
 
+export function createJWT ({address, signer}, payload, callback) {
+  const signingInput = createUnsignedToken(JOSE_HEADER, payload)
+
+  signer(signingInput, (error, signature) => {
+    if (error) return callback(error)
+    callback(null, [signingInput, signature].join('.'))
+  })
 }
 
 export function verifyJWT (jwt, callback) {
   // 1. decode jwt
+  const {payload} = decodeToken(jwt)
   // 2. Fetch uport-registry profile for iss
   // 3. verify signature using public key
   // 4. return payload in callback
+  callback(null, payload)
   // 5. return error in callback on any failure of the above
-
 }
--- src/SimpleSigner.js
+++ src/SimpleSigner.js
@@ -1,6 +1,9 @@
+import { SECP256K1Client } from 'json-tokens'
 
-export default function SimpleSigner(privateKey) {
-  return function (data, callback) {
-    // TODO implement signing
+export default function SimpleSigner (privateKey) {
+  return (data, callback) => {
+    const hash = SECP256K1Client.createHash(data)
+    const signature = SECP256K1Client.signHash(hash, privateKey)
+    callback(null, signature)
   }
 }
