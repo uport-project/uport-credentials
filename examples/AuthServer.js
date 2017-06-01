68066607cdd96c65a2b6290ff05c78ba1f05cc4c
Zach Ferland <zachferland@gmail.com>
2017-06-01 13:52:51 -0400

    fix: create working example

--- examples/AuthServer.js
+++ examples/AuthServer.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const uport = require('../lib/index.js')
+const uport = require('./lib/index.js')
 const bodyParser = require('body-parser')
+const decodeToken = require('jsontokens').decodeToken
+
+const redisOpts = 'your-redis-host:port'
 
 const app = express()
 app.use(bodyParser.json({ type: '*/*' }))
@@ -9,43 +12,51 @@ const signer = uport.SimpleSigner('955d253bca62bf380e34a36ed3802895e4adc297fa861
 const authCredentials = new uport.AuthCredentials({
   appName: 'Credential Tutorial',
   address: '0x15bcc15904f2317365cdb6beaf473b2315405763',
-  signer: signer
+  signer: signer,
+  challengeStorage: new uport.Storage('challenge', redisOpts),
+  responseStorage: new uport.Storage('response', redisOpts)
 })
 
 app.get('/authrequest', (req, res) => {
     // Browser requests to start authentication and receives a request token in response
-    credentials.createRequest({
+    authCredentials.createRequest({
       request: ['name'],
       // A reachable ip address
-      callbackUrl: 'http://192.168.1.101:8081/authResponse'
+      callbackUrl: 'your-callback-url-to-receive-responses'
     }).then( (requestToken) => {
-      const uri = 'me.uport:me?requestToken=' + requestToken
-      const.log(uri)
-      res.send(uri);
+      const token = decodeToken(requestToken)
+      const challenge = token.payload.challenge
+      res.send({message: requestToken, error:'', pairId: challenge.split('.')[0] });
     })
 })
 
-app.post('/authresponse', (req, res) => {
+app.use('/auth', express.static('./examples/AuthPage.html'))
+
+app.post('/auth', (req, res) => {
   // Mobile app posts response to challenge here
   const jwt = req.body.access_token
-  const response = 'Successful Authentication'
-  receive(jwt, response).then( credentials => {
-    console.log(response)
+  const token = decodeToken(jwt)
+  const response = {message: {status: 'Authentication Succesful'}, error: '' }
+  return authCredentials.receive(jwt, response).then(credentials => {
+    console.log('Authentication Success')
+    res.send(credentials)
   }).catch(err => {
     console.log('Authentication Failed')
   })
 })
 
-app.get('/authresponse/:pairID', (req, res) => {
+app.get('/authresponse/:pairId', (req, res) => {
   // Browser polling for a response
-  const pairID = req.params.pairId
-  authResponse(pairId).then(res => {
-    res.send(res)
+  const pairId = req.params.pairId
+  authCredentials.authResponse(pairId).then(response => {
+    if (response === null ) { res.status(404) }
+    res.send(response)
   }).catch(err => {
+    console.log(err)
     res.status(500)
   })
 })
 
-const server = app.listen(8081, () => {
+const server = app.listen(process.env.PORT || 8081, () => {
   console.log("AuthServer example app running...")
 })
