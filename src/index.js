8cb898f45843e73c54d91e8a93ce97a9df265144
Rob Whitaker <rob.whitaker.18@gmail.com>
2018-09-12 13:33:28 -0400

    feat: export normalizeDID function from did-jwt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Credentials from './Credentials'
 import { createJWT, verifyJWT } from './JWT'
 import { SimpleSigner } from 'did-jwt'
+import { normalizeDID } from 'did-jwt/lib/JWT'
 import { Contract, ContractFactory } from './Contract'
 import UportDIDResolver from 'uport-did-resolver'
 
 const JWT = { createJWT: createJWT , verifyJWT: verifyJWT }
 
-module.exports = { Credentials, SimpleSigner, Contract, ContractFactory, JWT }
+module.exports = { Credentials, SimpleSigner, Contract, ContractFactory, JWT, normalizeDID }
