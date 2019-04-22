4873156922d28d2fac7afe3d8501c02cfe4ced8a
Pelle Braendgaard <pelle@stakeventures.com>
2019-04-22 16:23:02 -0400

    more typedoc fixes

--- src/Credentials.ts
+++ src/Credentials.ts
@@ -306,8 +306,6 @@ class Credentials {
    * ```
    * 
    * @returns {Object} keypair
-   *           - {String} keypair.did         An ethr-did string for the new identity
-   *           - {String} keypair.privateKey  The identity's private key, as a string
    */
   static createIdentity(): Identity {
     const kp = secp256k1.genKeyPair()
@@ -331,15 +329,7 @@ class Credentials {
    * ```
    *
    *  @param    {Object}             [params={}]           request params object
-   *  @param    {Array}              params.requested      an array of attributes for which you are requesting credentials to be shared for
-   *  @param    {Array}              params.verified       an array of attributes for which you are requesting verified credentials to be shared for
-   *  @param    {Boolean}            params.notifications  boolean if you want to request the ability to send push notifications
-   *  @param    {String}             params.callbackUrl    the url which you want to receive the response of this request
-   *  @param    {String}             params.networkId      network id of Ethereum chain of identity eg. 0x4 for rinkeby
-   *  @param    {String}             params.rpcUrl         JSON RPC url for use with account connecting to non standard (private or permissioned chain). The JSON-RPC url must match the `networkId`
-   *  @param    {String[]}           params.vc             An array of JWTs about the requester, signed by 3rd parties
-   *  @param    {String}             params.accountType    Ethereum account type: "general", "segregated", "keypair", or "none"
-   *  @param    {Number}             expiresIn             Seconds until expiry
+   *  @param    {number}             expiresIn             Seconds until expiry
    *  @return   {Promise<Object, Error>}                   a promise which resolves with a signed JSON Web Token or rejects with an error
    */
   createDisclosureRequest(params: DisclosureRequestParams = {}, expiresIn = 600) {
